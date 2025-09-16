3a1f867b6b3b39505462cf0e4ba10ec868f81bcf
luizhypolito <luizfernandohypolito@gmail.com>
2025-09-16 17:30:34 -0300

    Add files via upload

---
+++ LP2-1609/q2.js
@@ -0,0 +1,14 @@
+let sangues = [];
+
+
+for (let i = 0; i < 10; i++) {
+
+    let tipos = prompt("Digite o tipo sanguíneo: "); 
+    precos.push(tipos);
+
+}
+
+for(let i = 0; i < 10; i++) {
+    
+
+}
