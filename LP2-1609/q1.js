da427d481253914b7f78687c592f431bed321d00
luizhypolito <luizfernandohypolito@gmail.com>
2025-09-18 14:14:22 -0300

    Update q1.js

--- LP2-1609/q1.js
+++ LP2-1609/q1.js
@@ -7,11 +7,12 @@ for (let i = 0; i < 5; i++) {
 }
 
 
-let precomasicaro = Math.max(...precos); //Math.max
+let precomasicaro = Math.max(...precos); //Math.max é uma biblioteca que capta o maior número de uma sequências 
 let indicemaiscaro = precos.indexOf(precomasicaro);
 
 let precomenoscaro = Math.min(...precos);
-let indicemenoscaro = precos.indexOf(precomenoscaro);
+let indicemenoscaro = precos.indexOf(precomenoscaro);  //Math.min é uma biblioteca que capta o menor número de uma sequências 
 
 console.log("O preço mais caro pertence ao produto ", indicemaiscaro);
+
 console.log("O preço mais barato pertence ao produto ", indicemenoscaro);
